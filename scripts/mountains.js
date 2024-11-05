5c1eff2bbcb356900b2061ad1160f06f06f5d5ea
Eshei Jenkins <eshjenk@gmail.com>
2024-11-05 10:53:28 -0500

    populated mountains dropdown in JS and added a card to html

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,7 +1,24 @@
-"use strict";
+let mountainsDropdown = document.getElementById("mountains");
+// let detailsDiv = document.getElementById("detailsDiv");
 
 function loadMountains(params) {
   for (const mountain of mountainsArray) {
-    console.log(mountain);
+    // console.log(mountain);
   }
 }
+loadMountains();
+
+function populateDrop() {
+  for (const mountain of mountainsArray) {
+    let option = document.createElement("option");
+    option.value = mountain.name;
+    option.innerText = mountain.name;
+    mountainsDropdown.appendChild(option);
+  }
+}
+
+populateDrop();
+
+// function filterByName() {
+//   let selectedMountain = mountainsDropdown.value;
+//   mountains.innerHTML = "";
