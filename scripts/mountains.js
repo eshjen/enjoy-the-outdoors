7c3b401201c8b63e6d227f9482dd18b3cf9b1f0c
Eshei Jenkins <eshjenk@gmail.com>
2024-11-08 10:19:09 -0500

    adding comments

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,13 +1,14 @@
 let mountainsDropdown = document.getElementById("mountains");
-// let detailsDiv = document.getElementById("detailsDiv");
+let detailsDiv = document.getElementById("mountainContainer");
 
 function loadMountains(params) {
   for (const mountain of mountainsArray) {
-    // console.log(mountain);
+    console.log(mountain);
   }
 }
 loadMountains();
 
+//creating function to populate the mountains dropdown with mountain names
 function populateDrop() {
   for (const mountain of mountainsArray) {
     let option = document.createElement("option");
@@ -17,8 +18,46 @@ function populateDrop() {
   }
 }
 
+// populateDrop();
+// creating a function that creates a card that displays info of the selected mountain
+function createMountainCard(mountainName) {
+  
+  const mountain = mountainsArray.filter((m) => m.name === mountainName)[0];
+  
+  console.log(mountain);
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const cardTitle = document.createElement("h5");
+  cardTitle.innerText = mountain.name;
+
+  const cardDesc = document.createElement("p");
+  cardDesc.innerText = mountain.desc;
+
+  const cardElevation = document.createElement("h6");
+  cardElevation.innerText = "Elevation: " + mountain.elevation;
+
+  const cardImage = document.createElement("img");
+  cardImage.src = "./images/" + mountain.img;
+  cardImage.alt = mountain.name;
+  console.log(cardImage.src);
+
+  card.appendChild(cardImage);
+  card.appendChild(cardTitle);
+  card.appendChild(cardDesc);
+  card.appendChild(cardElevation);
+  
+// displaying the card
+  detailsDiv.innerHTML = "";
+  detailsDiv.appendChild(card);
+  let mountainCard = document.querySelector("#mountainContainer");
+  mountainCard.style.display = "block";
+}
+
 populateDrop();
 
-// function filterByName() {
-//   let selectedMountain = mountainsDropdown.value;
-//   mountains.innerHTML = "";
+// adding event listener to change the value of the selected option when a new mountain is selected
+mountainsDropdown.addEventListener("change", function () {
+  const selectedMountain = mountainsDropdown.value;
+  createMountainCard(selectedMountain);
+});
